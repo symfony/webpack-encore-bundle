2a360fef1560db9ae910f21a5b920e67d0f0e2a1
Ryan Weaver <ryan@thatsquality.com>
2018-10-26 11:13:34 -0400

    Updating phpcs to fix license headers

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -1,10 +1,8 @@
 <?php
 
 /*
- * This file is part of the Symfony MakerBundle package.
- *
+ * This file is part of the Symfony WebpackEncoreBundle package.
  * (c) Fabien Potencier <fabien@symfony.com>
- *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
