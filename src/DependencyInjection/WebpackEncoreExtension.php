b22f685d76964ffe2100a4812683db262b4ea072
Maciej Kobus <maciej.kobus@ez.no>
2019-03-04 09:58:49 +0100

    Fixed issue with invalid cache keys for builds

--- src/DependencyInjection/WebpackEncoreExtension.php
+++ src/DependencyInjection/WebpackEncoreExtension.php
@@ -20,6 +20,8 @@ use Symfony\WebpackEncoreBundle\Asset\EntrypointLookup;
 
 final class WebpackEncoreExtension extends Extension
 {
+    private const ENTRYPOINTS_FILE_NAME = 'entrypoints.json';
+
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader($container, new FileLocator(\dirname(__DIR__).'/Resources/config'));
@@ -32,11 +34,11 @@ final class WebpackEncoreExtension extends Extension
             '_default' => $this->entrypointFactory($container, '_default', $config['output_path'], $config['cache']),
         ];
         $cacheKeys = [
-            '_default' => $config['output_path'].'/entrypoints.json',
+            '_default' => $config['output_path'].'/'.self::ENTRYPOINTS_FILE_NAME,
         ];
         foreach ($config['builds'] as $name => $path) {
             $factories[$name] = $this->entrypointFactory($container, $name, $path, $config['cache']);
-            $cacheKeys[rawurlencode($name)] = $path;
+            $cacheKeys[rawurlencode($name)] = $path.'/'.self::ENTRYPOINTS_FILE_NAME;
         }
 
         $container->getDefinition('webpack_encore.entrypoint_lookup.cache_warmer')
@@ -49,7 +51,7 @@ final class WebpackEncoreExtension extends Extension
     private function entrypointFactory(ContainerBuilder $container, string $name, string $path, bool $cacheEnabled): Reference
     {
         $id = sprintf('webpack_encore.entrypoint_lookup[%s]', $name);
-        $arguments = [$path.'/entrypoints.json', $cacheEnabled ? new Reference('webpack_encore.cache') : null, $name];
+        $arguments = [$path.'/'.self::ENTRYPOINTS_FILE_NAME, $cacheEnabled ? new Reference('webpack_encore.cache') : null, $name];
         $container->setDefinition($id, new Definition(EntrypointLookup::class, $arguments));
 
         return new Reference($id);
